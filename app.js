656c10856c9c96b05a42c3162a5e544e4dee7326
Your Name <you@example.com>
2024-01-17 16:16:46 +0100

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // javascript  responsive navigation menu
 const menuBtn = document.querySelector(".menu-btn");
+const navigation  = document.querySelector(".navigation")
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
-})
+    navigation.classList.toggle("active");
+});
 
 document.addEventListener('scroll', () => {
     const header = document.querySelector('header');
